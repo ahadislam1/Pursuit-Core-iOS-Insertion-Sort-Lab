77f1cbce17ab5fe44af564c13b8a60172ec817f8
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-10-15 15:56:45 -0400

    Yeah i can't figure out question two.

--- Pursuit-Core-Insertion-Sort-Lab/InsertionSortQuestions.swift
+++ Pursuit-Core-Insertion-Sort-Lab/InsertionSortQuestions.swift
@@ -2,17 +2,29 @@ import Foundation
 
 // Question One
 // Implement insertion sort that accepts a closure about how to sort elements
+
+
 func insertionSorted<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
+    var arr = arr
+    for indexOne in 1..<arr.count {
+        var indexTwo = indexOne
+        while indexTwo > 0 && !isSorted(arr[indexTwo - 1], arr[indexTwo]) {
+            arr.swapAt(indexTwo - 1, indexTwo)
+            indexTwo -= 1
+        }
+    }
     return arr
 }
+
+
 
 // Question Two
 // Sort an array of Ints using insertion sort without changing the position of any negative numbers
 // https://www.geeksforgeeks.org/sort-an-array-without-changing-position-of-negative-numbers/
 func insertionSortWithoutMovingNegatives<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
-    return arr
+    var arr = arr[Int(T)]
+    if arr.contains(0)
 }
-
 // Question Three
 // Implement insertion sort in place
 func insertionSort<T: Comparable>(arr: inout [T], by isSorted: (T, T) -> Bool) -> [T] {
